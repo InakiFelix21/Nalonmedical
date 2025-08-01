a96b36a6489cce3db79b03ad87ba72d2df716071
Iñaki Felix <inaki.felix21@gmail.com>
2025-08-01 12:13:11 -0300

    primer commit

---
+++ assets/js/main.js
@@ -0,0 +1,144 @@
+(function ($) {
+    "use strict";
+
+    $(document).ready(function ($) {
+        // Testimonial sliders
+        $(".testimonial-sliders").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            responsive: {
+                0: { items: 1, nav: false },
+                600: { items: 1, nav: false },
+                1000: { items: 1, nav: false, loop: true }
+            }
+        });
+
+        // Homepage slider
+        $(".homepage-slider").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            nav: true,
+            dots: false,
+            navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+            responsive: {
+                0: { items: 1, nav: false, loop: true },
+                600: { items: 1, nav: true, loop: true },
+                1000: { items: 1, nav: true, loop: true }
+            }
+        });
+
+        // Logo carousel
+        $(".logo-carousel-inner").owlCarousel({
+            items: 4,
+            loop: true,
+            autoplay: true,
+            margin: 30,
+            responsive: {
+                0: { items: 1, nav: false },
+                600: { items: 3, nav: false },
+                1000: { items: 4, nav: false, loop: true }
+            }
+        });
+
+
+        // Mean menu
+        $('.main-menu').meanmenu({ meanMenuContainer: '.mobile-menu', meanScreenWidth: "992" });
+
+        // Mean menu
+        $('.main-menu-contact').meanmenu({ meanMenuContainer: '.mobile-menu-contact', meanScreenWidth: "992" });
+
+    });
+
+}(jQuery));
+
+document.addEventListener('DOMContentLoaded', function () {
+    var widgetTitles = document.querySelectorAll('.footer-box .widget-title');
+    
+    widgetTitles.forEach(function (title) {
+        title.addEventListener('click', function () {
+            var parentBox = this.parentElement;
+            parentBox.classList.toggle('active');
+        });
+    });
+});
+
+    document.addEventListener('DOMContentLoaded', function() {
+        if (window.innerWidth <= 768) { // Ajusta el valor según tus necesidades
+            document.querySelectorAll('footer-box .widget-title').forEach(title => {
+                title.addEventListener('click', function() {
+                    const parentBox = this.parentElement;
+                    parentBox.classList.toggle('active');
+                });
+            });
+        }
+    });
+
+    document.addEventListener('DOMContentLoaded', function() {
+        if (window.innerWidth <= 768) { // Ajusta el valor según tus necesidades
+            document.querySelectorAll('.widget-title-contacto').forEach(title => {
+                title.addEventListener('click', function() {
+                    const parentBox = this.parentElement;
+                    parentBox.classList.toggle('active-contacto');
+                });
+            });
+        }
+    });
+
+    document.addEventListener('DOMContentLoaded', function() {
+        if (window.innerWidth <= 768) { // Ajusta el valor según tus necesidades
+            document.querySelectorAll('.widget-title-ayuda').forEach(title => {
+                title.addEventListener('click', function() {
+                    const parentBox = this.parentElement;
+                    parentBox.classList.toggle('active-ayuda');
+                });
+            });
+        }
+    });
+
+    document.addEventListener('DOMContentLoaded', function() {
+        if (window.innerWidth <= 768) { // Ajusta el valor según tus necesidades
+            document.querySelectorAll('.widget-title-suscribite').forEach(title => {
+                title.addEventListener('click', function() {
+                    const parentBox = this.parentElement;
+                    parentBox.classList.toggle('active-suscribite');
+                });
+            });
+        }
+    });
+
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+    }
+    
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    }
+
+        // Llama a la función al cargar la página
+        updateTextContent();
+    
+        // Llama a la función cada vez que se redimensiona la ventana
+        window.addEventListener('resize', updateTextContent);
+
+    function updateTextContent() {
+        var h3 = document.querySelector('.content-column-h3');
+        var span = document.querySelector('.h3-span');
+        var windowWidth = window.innerWidth;
+    
+        if (windowWidth <= 768) { // Cambia el texto en pantallas menores a 768px
+            h3.innerHTML = 'Tecnología <span class="h3-span"> Blow-Fill-Seal</span>'; // Texto combinado con <span>
+            span.style.display = 'none'; // Asegúrate de que el <span> se muestre
+        } else {
+            h3.textContent = 'Tecnología'; // Texto original del h3
+            span.textContent = 'Blow-Fill-Seal'; // Texto original del span
+            span.style.display = 'inline'; // Asegúrate de que el <span> se muestre
+        }
+    }
+        // Llama a la función al cargar la página
+        updateTextContent();
+    
+        // Llama a la función cada vez que se redimensiona la ventana
+        window.addEventListener('resize', updateTextContent);
+
---
+++ assets/js/translate.js
@@ -0,0 +1,237 @@
+// const translations = {     
+//     es: {
+//         title: '<span class="hero-title-main">PASIÓN POR LA CALIDAD.</span><span class="hero-text-span"><br> TECNOLOGÍA EN SOLUCIONES PARENTERALES</span>',
+//         nav: {
+//             about: 'SOBRE NOSOTROS',
+//             products: 'PRODUCTOS',
+//             qualityUnit: 'UNIDAD DE CALIDAD',
+//             news: 'NOVEDADES',
+//             contact: 'CONTACTO'
+//         },
+//         productSection: {
+//             sectionTitle: 'Nuestra empresa'
+//         },
+//         infoInicio: {
+//             missionTitle: 'Misión',
+//             missionText: 'Ser la compañía farmacéutica líder del sector de soluciones parenterales comprometida con mejorar la salud y el bienestar de los pacientes, actuando con responsabilidad, profesionalidad y compromiso, haciendo énfasis permanente en la excelencia, con los pacientes en el centro de todos nuestros esfuerzos.',
+            
+//             valuesTitle: 'Valores',
+//             valuesText: 'La labor de Tecsolpar Laboratorios se sustenta en valores perdurables, que avalan su misión y le permiten alcanzar los propósitos de su visión.',
+//             valuesSpan: 'Profesionalidad, Responsabilidad social, Calidad y Orientación al paciente y su bienestar.',
+//             valuesBtn: 'Ver más',
+            
+//             visionTitle: 'Visión',
+//             visionText: 'Ser reconocidos como referentes de excelencia y calidad en el sector farmacéutico nacional e internacional, evolucionando y adaptándonos a los constantes cambios y desafíos que las tecnologías y la investigación nos plantean.'
+//         },
+//         features: {
+//             paragraph: 'Tecsolpar Laboratorios innova incorporando a su vademécum soluciones inyectables en ampollas plásticas de última generación, que brindan un alto grado de seguridad, fiabilidad y comodidad en el manejo del envase en presentaciones de 5ml y 10ml.',
+//             title: 'Tecnología',
+//             btn: 'Saber más',
+//             text: 'Soluciones parenterales de gran volumen',
+
+//             industry: 'Industria nacional',
+//             national: 'Empresa 100% nacional',
+//             products: 'Productos',
+//             seeMore: 'Ver más',
+//             workTeam: 'Equipo de trabajo',
+//             employees: '+200 trabajadores'
+//         },
+//         cartBanner: {
+//             text: 'Tecsolpar Laboratorios fue pionero en la implementación de la tecnología form, fill, seal para la elaboración de sus especialidades farmacéuticas de gran volumen en sistema cerrado en el país. Desde el año 2008, viene invirtiendo constantemente en tecnología de punta adelantándose, incluso, a las normativas nacionales en materia de envases primarios.',
+//             title: 'Tecnología',
+//             tech: 'Form-Fill-Seal',
+//             btn: 'Saber más',
+//             paragraph: 'Soluciones inyectables de pequeño volumen'
+//         },
+//         footer: {
+//             pagesTitle: 'Páginas',
+//             home: 'Home',
+//             about: 'Sobre nosotros',
+//             products: 'Productos',
+//             qualityUnit: 'Unidad de calidad',
+//             contact: 'Contacto',
+//             news: 'Novedades',
+
+//             contactoTitle: 'Contacto',
+//             mail: 'Mail: tecsolpar@gmail.com',
+//             phone: 'Telefono: 112345678',
+//             instagram: 'Instagram: tecsolparlaboratorios',
+
+//             helpTitle: 'Central de ayuda',
+//             cookiesPolicy: 'Política de cookies',
+//             healthInfo: 'Información sanitaria',
+//             termsConditions: 'Condiciones generales',
+//             legalNotice: 'Aviso legal',
+
+//             subscribeTitle: 'Suscribite',
+//             subscribeText: 'Entérese de todas las novedades.',
+//             emailPlaceholder: 'Email'
+//         }
+//     },
+//     en: {
+//             title: '<span class="hero-title-main">PASSION FOR QUALITY.</span><span class="hero-text-span"><br> TECHNOLOGY IN PARENTERAL<br>SOLUTIONS</span>',
+//             nav: {
+//                 about: 'ABOUT US',
+//                 products: 'PRODUCTS',
+//                 qualityUnit: 'QUALITY UNIT',
+//                 news: 'NEWS',
+//                 contact: 'CONTACT'
+//             },
+//             productSection: {
+//                 sectionTitle: 'Our Company'
+//             },
+//             infoInicio: {
+//                 missionTitle: 'Mission',
+//                 missionText: 'To be the leading pharmaceutical company in the parenteral solutions business, committed to improving the health and well- being of patients by acting with responsibility, professionalism, and dedication. We place a permanent emphasis on excellence, with patients at the center of all our efforts.',
+//                 valuesTitle: 'Values',
+//                 valuesText: 'The work of Tecsolpar Labs is grounded in enduring values that support its mission and enable it to achieve its vision.',
+//                 valuesSpan: 'These values include professionalism, social responsibility, quality, and a focus on patients and their well-being.',
+//                 valuesBtn: 'See More',
+//                 visionTitle: 'Vision',
+//                 visionText: 'To be recognized as benchmarks of excellence and quality in the national and international pharmaceutical sectors, continuously evolving and adapting to the constant changes and challenges presented by new technologies and research.'
+//             },
+//             features: {
+//                 paragraph: 'Tecsolpar Labs innovates by incorporating injectable solutions into its portfolio, offered in state-of- the-art plastic ampoules that provide a high degree of safety, reliability, and convenience in handling, available in 5ml and 10ml.',
+//                 title: 'Technology',
+//                 btn: 'Learn More',
+//                 text: 'Large volume parenteral solutions',
+
+//                 industry: 'National Industry',
+//                 national: '100% National Company',
+//                 products: 'Products',
+//                 seeMore: 'See More',
+//                 workTeam: 'Work Team',
+//                 employees: '+200 Employees'
+//             },
+//             cartBanner: {
+//                 text: 'Tecsolpar Labs was a pioneer in implementing form-fill-seal technology to produce large-volume pharmaceutical solutions using a closed system in the country. Since 2008, the company has consistently invested in cutting-edge technology, even anticipating local regulations regarding primary packaging specifications.',
+//                 title: 'Technology',
+//                 tech: 'Form-Fill-Seal',
+//                 btn: 'Learn More',
+//                 paragraph: 'Small volume injectable solutions'
+//             },
+//             footer: {
+//                 pagesTitle: 'Pages',
+//                 home: 'Home',
+//                 about: 'About Us',
+//                 products: 'Products',
+//                 qualityUnit: 'Quality Unit',
+//                 contact: 'Contact',
+//                 news: 'News',
+    
+//                 contactoTitle: 'Contact',
+//                 mail: 'Mail: tecsolpar@gmail.com',
+//                 phone: 'Phone: 112345678',
+//                 instagram: 'Instagram: tecsolparlaboratories',
+    
+//                 helpTitle: 'Help Center',
+//                 cookiesPolicy: 'Cookie Policy',
+//                 healthInfo: 'Health Information',
+//                 termsConditions: 'General Conditions',
+//                 legalNotice: 'Legal Notice',
+    
+//                 subscribeTitle: 'Subscribe',
+//                 subscribeText: 'Stay updated with the latest news.',
+//                 emailPlaceholder: 'Email'
+//             }
+//         }
+//     };
+    
+    // function translatePage(lang) {
+    //     document.getElementById("hero-title").innerHTML = translations[lang].title;
+    
+    //     // Translate features
+    //     document.getElementById("feature-paragraph").innerText = translations[lang].features.paragraph;
+    //     document.getElementById("feature-title").innerText = translations[lang].features.title;
+    //     document.getElementById("feature-btn").innerText = translations[lang].features.btn;
+    //     document.getElementById("feature-text").innerText = translations[lang].features.text;
+
+    //     document.getElementById("feature-industry").innerText = translations[lang].features.industry;
+    //     document.getElementById("feature-national").innerText = translations[lang].features.national;
+    //     document.getElementById("feature-products").innerText = translations[lang].features.products;
+    //     document.getElementById("feature-see-more").innerText = translations[lang].features.seeMore;
+    //     document.getElementById("feature-work-team").innerText = translations[lang].features.workTeam;
+    //     document.getElementById("feature-employees").innerText = translations[lang].features.employees;
+
+    //     // Tanslate footer
+    //     document.querySelector(".footer-box.pages .widget-title").innerText = translations[lang].footer.pagesTitle;
+    //     document.querySelector(".footer-box.pages ul li:nth-child(1) a").innerText = translations[lang].footer.home;
+    //     document.querySelector(".footer-box.pages ul li:nth-child(2) a").innerText = translations[lang].footer.about;
+    //     document.querySelector(".footer-box.pages ul li:nth-child(3) a").innerText = translations[lang].footer.products;
+    //     document.querySelector(".footer-box.pages ul li:nth-child(4) a").innerText = translations[lang].footer.qualityUnit;
+    //     document.querySelector(".footer-box.pages ul li:nth-child(5) a").innerText = translations[lang].footer.contact;
+    //     document.querySelector(".footer-box.pages ul li:nth-child(6) a").innerText = translations[lang].footer.news;
+            
+    //     document.querySelector(".footer-box.footer-box-contacto .widget-title-contacto").innerText = translations[lang].footer.contactoTitle;
+    //     document.querySelector(".footer-box.footer-box-contacto ul li:nth-child(1) a").innerText = translations[lang].footer.mail;
+    //     document.querySelector(".footer-box.footer-box-contacto ul li:nth-child(2) a").innerText = translations[lang].footer.phone;
+    //     document.querySelector(".footer-box.footer-box-contacto ul li:nth-child(3) a").innerText = translations[lang].footer.instagram;
+            
+    //     document.querySelector(".footer-box.about-widget .widget-title").innerText = translations[lang].footer.helpTitle;
+    //     document.querySelector(".footer-box.about-widget ul li:nth-child(1) a").innerText = translations[lang].footer.cookiesPolicy;
+    //     document.querySelector(".footer-box.about-widget ul li:nth-child(2) a").innerText = translations[lang].footer.healthInfo;
+    //     document.querySelector(".footer-box.about-widget ul li:nth-child(3) a").innerText = translations[lang].footer.termsConditions;
+    //     document.querySelector(".footer-box.about-widget ul li:nth-child(4) a").innerText = translations[lang].footer.legalNotice;
+            
+    //     document.querySelector(".footer-box.subscribe .widget-title-suscribite").innerText = translations[lang].footer.subscribeTitle;
+    //     document.querySelector(".footer-box.subscribe p").innerText = translations[lang].footer.subscribeText;
+    //     document.querySelector(".footer-box.subscribe input[type=email]").placeholder = translations[lang].footer.emailPlaceholder;
+
+    //     // Translate info-inicio section
+    //     document.querySelector(".product-section .section-title h3").innerText = translations[lang].productSection.sectionTitle;
+
+    //     document.querySelector(".info-post h3").innerText = translations[lang].infoInicio.missionTitle;
+    //     document.querySelector(".info-post p").innerText = translations[lang].infoInicio.missionText;
+    //     document.querySelector(".info-post-medio h3").innerText = translations[lang].infoInicio.valuesTitle;
+    //     document.querySelector(".info-post-medio p").innerText = translations[lang].infoInicio.valuesText;
+    //     document.querySelector(".info-post-medio .span-info-medio").innerText = translations[lang].infoInicio.valuesSpan;
+    //     document.querySelector(".info-post-medio a").innerText = translations[lang].infoInicio.valuesBtn;
+    //     document.querySelector(".info-post-derecha h3").innerText = translations[lang].infoInicio.visionTitle;
+    //     document.querySelector(".info-post-derecha p").innerText = translations[lang].infoInicio.visionText;
+    
+    //     // Translate cart-banner
+    //     document.getElementById("cart-banner-text").innerText = translations[lang].cartBanner.text;
+    //     document.getElementById("cart-banner-title").innerText = translations[lang].cartBanner.title;
+    //     document.getElementById("cart-banner-tech").innerText = translations[lang].cartBanner.tech;
+    //     document.getElementById("cart-banner-btn").innerText = translations[lang].cartBanner.btn;
+    //     document.getElementById("cart-banner-paragraph").innerText = translations[lang].cartBanner.paragraph;
+    
+    //     // Translate navigation
+    //     document.querySelector('.nav-text a[href="./about.html"]').innerText = translations[lang].nav.about;
+    //     document.querySelector('.nav-text a[href="./productos.html"]').innerText = translations[lang].nav.products;
+    //     document.querySelector('.nav-text a[href="./unidad.html"]').innerText = translations[lang].nav.qualityUnit;
+    //     document.querySelector('.nav-text a[href="./novedades.html"]').innerText = translations[lang].nav.news;
+    //     document.querySelector('.nav-text a[href="./contact.html"]').innerText = translations[lang].nav.contact;
+    
+    //     // Translate responsive sidenav
+    //     document.querySelector('#mySidenav .links-container a[href="./about.html"]').innerText = translations[lang].nav.about;
+    //     document.querySelector('#mySidenav .links-container a[href="./productos.html"]').innerText = translations[lang].nav.products;
+    //     document.querySelector('#mySidenav .links-container a[href="./unidad.html"]').innerText = translations[lang].nav.qualityUnit;
+    //     document.querySelector('#mySidenav .links-container a[href="./novedades.html"]').innerText = translations[lang].nav.news;
+    //     document.querySelector('#mySidenav .links-container a[href="./contact.html"]').innerText = translations[lang].nav.contact;
+    // }
+
+    function updateTextContent() {
+        var h3 = document.querySelector('.content-column-izq-h3');
+        var span = document.querySelector('.h3-span-izq');
+        var windowWidth = window.innerWidth;
+    
+        if (windowWidth <= 768) { // Cambia el texto en pantallas menores a 768px
+            h3.innerHTML = 'Tecnología <span class="h3-span-izq"> Form-Fill-Seal</span>'; // Texto combinado con <span>
+            span.style.display = 'none'; // Asegúrate de que el <span> se muestre
+        } else {
+            h3.textContent = 'Tecnología'; // Texto original del h3
+            span.textContent = 'Form-Fill-Seal'; // Texto original del span
+            span.style.display = 'inline'; // Asegúrate de que el <span> se muestre
+        }
+    }
+
+    
+
+    // Llama a la función al cargar la página
+    updateTextContent();
+    
+    // Llama a la función cada vez que se redimensiona la ventana
+    window.addEventListener('resize', updateTextContent);
+
+    document.getElementById('custom-translate-btn').addEventListener('click', triggerGoogleTranslate);
